c78cebf62fa879469071f40cfa3c1f0ff384fdf6
Akib <akib3366@gmail.com>
2021-06-26 16:59:47 +0600

    queue inplemented

--- datastruct-implementations/4-queue.js
+++ datastruct-implementations/4-queue.js
@@ -5,29 +5,29 @@ class Node {
     }
 }
 
-class Stack {
+class Queue {
     constructor() {
         this.first = null;
         this.last = null;
         this.size = 0;
     }
 
-    push(val) {
+    //like push
+    enqueue(val) {
         let node = new Node(val);
         if (!this.first) {
             this.first = node;
             this.last = node;
             this.size++;
         } else {
-            let node = new Node(val);
-            let temp = this.first;
-            this.first = node;
-            this.first.next = temp;
+            this.last.next = node;
+            this.last = node;
             this.size++;
         }
     }
 
-    pop() {
+    //like pop
+    dequeue() {
         if (this.size == 0) return null;
 
         let temp = this.first;
@@ -35,7 +35,6 @@ class Stack {
             this.last = null;
         }
         this.first = this.first.next;
-        temp.next = null;
         this.size--;
         return temp.val;
 
@@ -44,12 +43,12 @@ class Stack {
 }
 
 
-let s = new Stack();
-s.push(1);
-s.push(2);
-s.push(3);
+let s = new Queue();
+s.enqueue(1);
+s.enqueue(2);
+s.enqueue(3);
 
 
-console.log(s.pop());
-console.log(s.pop());
-console.log(s.pop());
+console.log(s.dequeue());
+console.log(s.dequeue());
+console.log(s.dequeue());
