14b9cf156eb2ff8c2401e64e741fca48fdbb2461
Akib <akib3366@gmail.com>
2021-06-27 15:17:42 +0600

    BFS on BST implemented

--- datastruct-implementations/5-BST.js
+++ datastruct-implementations/5-BST.js
@@ -55,9 +55,23 @@ class BST {
             }
             if (current == null) return false;
         }
+    }
 
+    BFS() {
+        let node = this.root;
+        let data = [];
+        let queue = [];
 
+        queue.push(node);
 
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        return data;
     }
 }
 
