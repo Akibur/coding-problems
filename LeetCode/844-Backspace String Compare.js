39b0b9db7af7960646ed1e2a139ccdeb3b48e120
Akib <akib3366@gmail.com>
2021-07-27 16:47:20 +0600

    844 updated

--- LeetCode/844-Backspace String Compare.js
+++ LeetCode/844-Backspace String Compare.js
@@ -3,19 +3,17 @@
 // Note that after backspacing an empty text, the text will continue empty.
 
 var backspaceCompare = function (s, t) {
-    var backspaceCompare = function (S, T) {
-        const handleBackSpace = (str) => {
-            let stack = [];
-            let i = 0;
-            while (i < str.length) {
-                if (str[i] === '#') stack.pop();
-                else stack.push(str[i]);
-                i++;
-            }
-            return stack.join('');
-        };
-        return handleBackSpace(S) === handleBackSpace(T);
+    const handleBackSpace = (str) => {
+        let stack = [];
+        let i = 0;
+        while (i < str.length) {
+            if (str[i] === '#') stack.pop();
+            else stack.push(str[i]);
+            i++;
+        }
+        return stack.join('');
     };
+    return handleBackSpace(S) === handleBackSpace(T);
 };
 
 let s = "ab#c", t = "ad#c";
