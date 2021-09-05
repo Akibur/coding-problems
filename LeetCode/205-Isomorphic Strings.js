57e45e74bfde983b7e9abd0c376bc424df0604f7
Akib <akib3366@gmail.com>
2021-09-05 13:48:32 +0600

    Isomorphic string solved

---
+++ LeetCode/205-Isomorphic Strings.js
@@ -0,0 +1,21 @@
+var isIsomorphic = function (s, t) {
+
+    let m = new Map();
+    for (let i = 0; i < s.length; i++) {
+        if (!m.has(s[i]))
+            m.set(s[i], t[i]);
+        else {
+
+            if (m.get(s[i]) != t[i]) {
+
+                return false;
+            }
+        }
+    }
+    return new Set([...m.values()]).size == m.size;
+
+
+};
+
+
+console.log(isIsomorphic("badc", "baba"));
